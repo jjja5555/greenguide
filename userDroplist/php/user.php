41cb49d7681067c7f60d96a4e2ff3e9c1b5eab91
R-en <renli@uw.edu>
2018-10-10 14:23:22 -0700

    Added navbar files

---
+++ userDroplist/php/user.php
@@ -0,0 +1,10 @@
+<?php
+<h2>Log in</h2>
+<form id="login" action="login.php" method="post">
+    <dl>
+        <dt>Name</dt>       <dd><input type="text" name="name" /></dd>
+        <dt>Password</dt>   <dd><input type="password" name="password" /></dd>
+        <dt> </dt>          <dd><input type="submit" value="Log in" /></dd>
+    </dl>
+</form>
+?php>
