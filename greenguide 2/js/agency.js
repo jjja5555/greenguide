24848e815a959ddb917b8e640bbee18b9480e775
MartinH <jjja5555@uw.edu>
2018-10-21 15:54:28 -0700

    added formto greenguide folder
    form

--- greenguide 2/js/agency.js
+++ greenguide 2/js/agency.js
@@ -32,7 +32,7 @@
 
 })(jQuery); // End of use strict
 
-//jQuery time
+
 var current_fs, next_fs, previous_fs; //fieldsets
 var left, opacity, scale; //fieldset properties which we will animate
 var animating; //flag to prevent quick multi-click glitches
---
+++ userDroplist/php/XiaoHuang_AS3.py
@@ -0,0 +1,4 @@
+f = open('voting_record_dump109.txt')
+mylist = list(f)
+
+#def create_voting_dict(strlist):
